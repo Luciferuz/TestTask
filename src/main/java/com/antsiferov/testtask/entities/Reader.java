c4a18addf4816925ac617d39449fff0385d571bd
Сергей <sergozas@yandex.ru>
2023-08-03 11:09:38 +0300

    #7 - Created entities

---
+++ src/main/java/com/antsiferov/testtask/entities/Reader.java
@@ -0,0 +1,29 @@
+package com.antsiferov.testtask.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Reader {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+}
